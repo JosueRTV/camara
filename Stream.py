aa7eb9b3db57272316f0f9761f20f713f56893c0
josueRTV <josue.rtv@gmail.com>
2019-02-22 03:44:15 -0600

    Agregando Stream

---
+++ Stream.py
@@ -0,0 +1,13 @@
+import cv2
+
+cap =  cv2.VideoCapture(0)
+
+while True: 
+    _, img = cap.read()
+    cv2.imshow('frame',img)
+    cv2.imshow('frame2',crop_img)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
